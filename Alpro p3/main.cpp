1ce9acb7902009cb6604df9d1fcefda4d0f2d804
bilacute <612202300095@mhs.dinus.ac.id>
2024-09-27 11:58:20 +0700

    Add files via upload

---
+++ Alpro p3/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+string nama, nim;
+int usia;
+
+int main()
+{
+    cout << "Masukkan nama: ";
+    cin >> nama;
+
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+
+    cout << "Masukkan usia: ";
+    cin >> usia;
+
+    cout << "Nama: " << nama << endl;
+    cout << "NIM: " << nim << endl;
+    cout << "Usia: " << usia << endl;
+
+    //for(int i = 0; i <usia; i++){
+        //cout << nama << endl;
+    //}
+
+    int i = 0;
+    do {
+        cout << nama <<endl;
+        i++;
+    }
+    while (1 < 20);
+
+    return 0;
+}
+
